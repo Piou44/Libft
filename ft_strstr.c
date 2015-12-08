265657bfe784900715d6f8feb882e58960fb82fe
Fanny Huang <FannyHuang@Fanny.local>
2015-12-08 23:03:02 +0100

    OK

--- ft_strstr.c
+++ ft_strstr.c
@@ -1,41 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/24 15:19:31 by fhuang            #+#    #+#             */
-/*   Updated: 2015/12/02 16:23:32 by fhuang           ###   ########.fr       */
+/*   Created: 2015/12/01 19:10:21 by fhuang            #+#    #+#             */
+/*   Updated: 2015/12/02 16:21:07 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <string.h>
+#include <stdio.h>
 char	*ft_strstr(const char *s1, const char *s2)
 {
-	int		i;
-	int		j;
-	int		tmp;
+	size_t		len;
+	int			i;
 
 	i = 0;
-	tmp = 0;
-	if (s2[i] == '\0')
+	len = ft_strlen(s2);
+	if (len == 0)
 		return ((char*)s1);
 	while (s1[i])
-	{
-		j = 0;
-		while (s1[i] && s1[i] != s2[j])
-			i++;
-		tmp = i;
-		while (s2[j] && s1[i++] == s2[j])
-			j++;
-		if (s2[j] == '\0')
-			return ((char*)s1 + tmp);
-		else if (s1[i])
-			i = tmp + 1;
-	}
-	if (s1[i] == '\0')
-		return (NULL);
-	return (0);
+		if (ft_strncmp(s1 + i++, s2, len) == 0)
+			return ((char*)s1 + i - 1);
+	return (NULL);
 }
